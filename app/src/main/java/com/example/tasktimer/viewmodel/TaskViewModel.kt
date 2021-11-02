f9b108479a30881476837dbac78c872f000cd177
Omari-hajar <89646158+Omari-hajar@users.noreply.github.com>
2021-11-02 20:30:54 +0300

    Database commit- hope I am doing it correctly

---
+++ app/src/main/java/com/example/tasktimer/viewmodel/TaskViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.tasktimer.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.tasktimer.database.TaskDatabase
+import com.example.tasktimer.model.Task
+import com.example.tasktimer.repository.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskViewModel(application: Application): AndroidViewModel(application){
+
+    val readAllTasks: LiveData<List<Task>>
+    val repository: Repository
+
+    init {
+        val taskDao = TaskDatabase.getDatabase(application).taskDao()
+        repository = Repository(taskDao)
+        readAllTasks = repository.readAllTasks
+    }
+
+    fun addTask(task: Task){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.addTask(task)
+        }
+    }
+
+    fun updateTask(task: Task){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.updateTask(task)
+        }
+    }
+
+    fun deleteTask(task: Task){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.deleteTask(task)
+        }
+    }
+
+}
